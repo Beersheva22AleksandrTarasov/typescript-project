bdeab149b2b274d5e3870a6bd526bcc07c36acd7
Alex Tarasov <tarasof.av@gmail.com>
2023-06-14 14:24:14 +0300

    commit before break

--- build/main.js
+++ build/main.js
@@ -1,3 +1,9 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var Rectangle_1 = __importDefault(require("./Rectangle"));
 var n = 10;
 n = 20;
 var num = 12;
@@ -17,4 +23,20 @@ Array.from(map1.entries()).forEach(function (e) { return console.log("".concat(e
 function f(a) {
     return typeof a == "number" ? a * 2 : +(a + 2);
 }
+var comp = function (num1, num2) {
+    return num1 - num2;
+};
+displayPerson({ id: 123, date: "2000-10-10", name: "Vasya" });
+function displayPerson(prs) {
+    var _a;
+    prs.gender && console.log((_a = prs.gender) === null || _a === void 0 ? void 0 : _a.substring(0, 3));
+}
+function cipher(test, key) {
+    return "";
+}
+function decipher(test, key) {
+    return "";
+}
+var shape = new Rectangle_1.default(3, 4);
+var width = shape.width;
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,52 @@
+import Rectangle from "./Rectangle";
+
 let n = 10;
 n = 20;
-let num:number | 'abc' = 12;
+let num: number | 'abc' = 12;
 num = 100;
 num = "abc";
-const ar:string[] = [];
+const ar: string[] = [];
 ar[0] = "kuku";
 ar[1] = "2";
 ar.push("abc")
-const ar1:[string, number?] =['abc', 10];
+const ar1: [string, number?] = ['abc', 10];
 ar1[0] = 'lmn'
-const ar2:Array<string> = [];
+const ar2: Array<string> = [];
 const map1: Map<string, number> = new Map([["abc", 40]]);
 console.log(map1.get("abc"));
 console.log(map1.entries());
 Array.from(map1.entries()).forEach(e => console.log(`${e[0]} -> ${e[1]}`));
-function f(a: string | number): number{
+function f(a: string | number): number {
     return typeof a == "number" ? a * 2 : +(a + 2);
 }
+type Comparator<T = number> = (p1: T, p2: T) => number;
+let comp: Comparator =
+    function (num1: number, num2: number): number {
+        return num1 - num2;
+    }
+type Person = {
+    id: number; date: Date | string, name: string,
+    gender?: "male" | "female"
+};
+
+displayPerson({ id: 123, date: "2000-10-10", name: "Vasya" })
+function displayPerson(prs: Person): void {
+    prs.gender && console.log(prs.gender?.substring(0, 3))
+}
+
+function cipher(test: string, key: number): string{
+    //TODO
+    //cipher("yz", 6) => " !"
+    return "";
+}
+function decipher(test: string, key: number): string{
+    //TODO
+    //decipher(" !", 6) => "yz"
+    return "";
+}
+
+const shape: Rectangle = new Rectangle(3, 4);
+let width = shape.width;
+
+
 
